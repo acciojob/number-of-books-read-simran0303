6b44b15c1763cd32cb4402910d1761fb125bdc60
simran0303 <128297136+simran0303@users.noreply.github.com>
2023-06-19 18:08:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count = 0 ;
+for(let i=0;i<library.length; i++){
+	library[i].readingStatus && count++;
+}
+return count;
 };
 
 // Do not change the code below
